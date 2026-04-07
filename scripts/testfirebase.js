2e8cec2d1ff8b52bfb2006eac5afca3387b6c38a
ArtemisByte <197433795+ArtemisByte@users.noreply.github.com>
2026-04-07 08:39:56 +0300

    Create testfirebase.js

---
+++ scripts/testfirebase.js
@@ -0,0 +1,17 @@
+// scripts/firebase.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDFdwOOzR9_AOldg4wY_j2G7aAk3mDr1Gk",
+  authDomain: "testing-8c578.firebaseapp.com",
+  projectId: "testing-8c578",
+  storageBucket: "testing-8c578.firebasestorage.app",
+  messagingSenderId: "196054524863",
+  appId: "1:196054524863:web:7affae6fa9435c8020370a",
+  measurementId: "G-23TQF73ZNK"
+};
+
+firebase.initializeApp(firebaseConfig);
+console.log("Firebase initialized:", firebase.apps[0]);
+
+//THIS FIREBASE API KEY WILL BE DISCONTINUED/INVALID//
+
+
